01fb66cdf569375aa02d8d8c36f65238444f9231
Bach Le <bach@bullno1.com>
2024-01-07 20:19:16 +0800

    Initial commit

---
+++ deps/cute_framework.cmake
@@ -0,0 +1,8 @@
+set(CF_FRAMEWORK_STATIC OFF CACHE BOOL "")
+set(CF_FRAMEWORK_BUILD_SAMPLES OFF CACHE BOOL "")
+set(CF_FRAMEWORK_BUILD_TESTS OFF CACHE BOOL "")
+add_subdirectory("cute_framework" EXCLUDE_FROM_ALL)
+target_include_directories(cute SYSTEM PUBLIC BEFORE "cute_framework/include")
+target_include_directories(cute SYSTEM PUBLIC BEFORE "cute_framework/libraries")
+target_include_directories(cute SYSTEM PUBLIC BEFORE "cute_framework/libraries/cimgui")
+target_include_directories(cute SYSTEM PUBLIC BEFORE "cute_framework/libraries/cimgui/imgui")
